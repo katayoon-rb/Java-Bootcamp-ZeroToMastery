e86600a6a1c19fa02d927aa88527aa1b01fde057
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-05 19:00:52 +0330

    Update Car.java

--- 22 constructors/exercise4constructorsandinheritance/Car.java
+++ 22 constructors/exercise4constructorsandinheritance/Car.java
@@ -1,4 +1,4 @@
-package constructors.exercises.exercise4constructorsandinheritance;
+package constructors.exercise4constructorsandinheritance;
 
 public class Car extends Vehicle {
     private String model;
