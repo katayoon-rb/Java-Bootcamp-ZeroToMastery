7e2e945a6be61a926adf63bbfbec42ee287a7581
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-05 11:03:00 +0100

    Add files via upload

---
+++ 04 packages and classes/exercise2inventory/inventory/Warehouse.java
@@ -0,0 +1,7 @@
+package packagesandclasses.exercise2inventory.inventory;
+
+public class Warehouse {
+    public int warehouseId;
+    public String warehouseName;
+    public String inventories;
+}
