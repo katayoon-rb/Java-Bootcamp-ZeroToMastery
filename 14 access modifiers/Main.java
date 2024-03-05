5465a23ec626aca8bd74d9fd1978d600e2912059
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-05 15:23:57 +0330

    Delete 14 access modifiers/Main.java

--- 14 access modifiers/Main.java
+++
@@ -1,9 +0,0 @@
-package accessmodifiers;
-
-public class Main {
-    public static void main(String[] args) {
-        Person p = new Person();
-        p.name = "Maria";
-        p.sayHi();
-    }
-}
