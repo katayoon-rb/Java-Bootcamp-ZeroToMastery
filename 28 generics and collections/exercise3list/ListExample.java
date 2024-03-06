d6d518332df34bf3ad6885f07ceb65f4829ea323
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-06 14:22:24 +0100

    Add files via upload

---
+++ 28 generics and collections/exercise3list/ListExample.java
@@ -0,0 +1,12 @@
+package genericsandcollections.exercises.exercise3list;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Muffin");
+        names.add("Snowflake");
+        names.add("Walnut");
+    }
+}
